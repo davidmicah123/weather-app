f61dddf7972879109813429c621d5821e34ddf96
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2022-07-25 03:11:43 +0100

    adding funstionality

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const displayResult = document.querySelector(".display_result");
+const mainInput = document.querySelector(".main_input");
+const forecast = document.querySelector(".weather");
+mainInput.focus();
 
+const getPosition = function () {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+getPosition().then((position) => {
+  console.log(position);
+});
