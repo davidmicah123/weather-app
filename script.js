e6b883ed0f2badbaebcd2beda317cad5f116ff97
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2022-07-25 03:13:54 +0100

    adding functionality

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ const getPosition = function () {
 getPosition().then((position) => {
   console.log(position);
 });
+
+getPosition();
+
+const displayFlag = async function () {
+  try {
+      // const country = await fetch(
+      //   `https://restcountries.com/v2/name/${mainInput.value}`
+      // );
+      // const country = await fetch(
+      //   `https://restcountries.com/v2/alpha/{code}`
+      // );
+      // const resp = await country.json();
+      // const data = await resp;
+      // console.log(data);
+      // const flag = await data[0].flag;
+      // console.log(flag);
